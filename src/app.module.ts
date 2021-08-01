bb951e73e4a159fe645d76db5275e7e1fba473f2
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-01 09:48:35 -0400

    Configurando banco de dados e TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MedicosModule } from './medicos/medicos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// A conexão com DB foi feita com Postgres co serviço em nuvem do elephatesql.com
+
+// Obs: Para conectar é preciso conectar a um SGBD, no meu caso estou usando o DBEAVER.
+// Para conectar ao DBEAVER, basta conectar usando as credenciais abaixo.
 @Module({
-  imports: [MedicosModule],
+  imports: [
+    MedicosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'kesavan.db.elephantsql.com',
+      port: 5432,
+      username: 'uewmvuls',
+      password: 'i5MuTbBnXtqNfiMQu5V-puRs2zs0fYNy',
+      database: 'uewmvuls',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
 })
 export class AppModule {}
